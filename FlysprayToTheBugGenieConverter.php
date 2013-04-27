01aff8dd4a9901f90dc87a21e20ad529a2588168
SjonHortensius <sjon@hortensius.net>
2013-04-27 15:44:59 +0300

    Added gpl block

--- FlysprayToTheBugGenieConverter.php
+++ FlysprayToTheBugGenieConverter.php
@@ -1,5 +1,23 @@
 #!/usr/bin/php
 <?php
+/*
+	(C) 2013 Sjon Hortensius
+
+	This file is part of FlysprayToTheBugGenieConverter.
+
+	FlysprayToTheBugGenieConverter is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	FlysprayToTheBugGenieConverter is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with FlysprayToTheBugGenieConverter.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 $fs = new PDO('mysql:host=localhost;dbname=flyspray', 'username', 'password');
 $fs->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
